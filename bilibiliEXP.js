1f4fc129e05ec8afb2f4b86c5fd2ef1f4b81d8fb
dazo66 <dazo66@dazo66.com>
2020-12-30 21:06:47 +0800

    update

--- bilibiliEXP.js
+++ bilibiliEXP.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name         bilibiliEXP
 // @namespace    dazo66
-// @version      1.3.2
+// @version      1.3.3
 // @description  自动完成b站的每日投币 每日分享和每日银瓜子换硬币
 // @author       dazo66
 // @homepage     https://github.com/dazo66/bilibiliEXP
@@ -34,8 +34,8 @@ function _post(path, params, hender, onload) {
     req.withCredentials = true;
     req.open("POST", path);
     hender(req)
-    req.setRequestHeader("Accept", "application/json, text/plain, */*")
-    req.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    //req.setRequestHeader("Accept", "application/json, text/plain, */*")
+    //req.setRequestHeader("Content-type","application/x-www-form-urlencoded");
     req.onreadystatechange = function() { // Call a function when the state changes.
         if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
             onload(req.responseText);
@@ -177,4 +177,6 @@ function run() {
     localStorage.setItem("lastDate", currentDate)
 };
 setTimeOut();
-run();
+window.setTimeout(function(){
+    run();
+}, 1000);
