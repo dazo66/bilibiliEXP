8ef5652bbdb0d0426141672f858ffee94f5be58b
dazo66 <dazo66@dazo66.com>
2020-12-30 21:17:19 +0800

    update

--- bilibiliEXP.js
+++ bilibiliEXP.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name         bilibiliEXP
 // @namespace    dazo66
-// @version      1.3.3
+// @version      1.3.4
 // @description  自动完成b站的每日投币 每日分享和每日银瓜子换硬币
 // @author       dazo66
 // @homepage     https://github.com/dazo66/bilibiliEXP
@@ -34,8 +34,7 @@ function _post(path, params, hender, onload) {
     req.withCredentials = true;
     req.open("POST", path);
     hender(req)
-    //req.setRequestHeader("Accept", "application/json, text/plain, */*")
-    //req.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+
     req.onreadystatechange = function() { // Call a function when the state changes.
         if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
             onload(req.responseText);
@@ -123,7 +122,10 @@ function autoSendCoin() {
                 if(multiply != 0) {
                     sendLog(`[自动投币]尝试投币 投币数量${multiply}`);
                     _post("https://api.bilibili.com/x/web-interface/coin/add", params,
-                    function(req){},
+                    function(req){
+                        //req.setRequestHeader("Accept", "application/json, text/plain, */*")
+                        req.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+                    },
                     function(responseText2) {
                         sendLog(responseText2);
                         sendedCoin = coin_num / 10 + multiply
